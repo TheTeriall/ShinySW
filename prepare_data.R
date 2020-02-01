f6974ccf4d5dd1fa44e341aa0187101bad19a846
Daniel Kreuz <daniel-kreuz1@web.de>
2020-02-01 14:22:15 +0100

    ENH: Created basic shiny template

---
+++ prepare_data.R
@@ -0,0 +1,10 @@
+
+library(tidyverse)
+library(plotly)
+library(magrittr)
+library(data.table)
+
+
+data <- read.csv("C:/Users/Dicke/OneDrive/Desktop/Summoners War Exporter Files/runs/Teriall-7993272-runs.csv")
+
+
---
+++ ui.R
@@ -0,0 +1,10 @@
+library(shiny)
+
+library(shiny)
+
+ui <- fluidPage(
+  
+  
+  
+)
+
